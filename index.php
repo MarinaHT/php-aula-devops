1168cc13ba5e1c12f354e2eb9c46eb4745b31349
Marina <marina.hamada@gmail.com>
2021-01-18 13:37:21 -0300

    Versionando meu primeiro código PHP

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color=blue; text align: center">
+<h1>
+A verdadeira abundância é encontrada na mente de amor. s2
+<br>
+Torne-se uma pessoa capaz de elogiar os outros na sua ausência!
+</h1>
+<br>
+</body>
+</html>
